82d6a5584713f76935957649f90b7170e9be79dd
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-16 21:29:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -265,7 +265,7 @@ end
 
 def long_name_steals_a_ton()
   long_player = player_with_longest_name()
-  long_rebound_player = 0
+  steals_player = 0
   most_steals = 0
   player_lib = game_hash()
   player_lib
